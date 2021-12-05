aacb75d537badda3788ae130abcd25d744a862e5
michaelFellows <83875947+michaelFellows@users.noreply.github.com>
2021-12-04 22:41:45 -0500

    Embedded the JS since it wasnt working as static

--- static/js/like-button.js
+++ static/js/like-button.js
@@ -11,18 +11,17 @@ $("form.like_button_class").on("submit", function (e) {
     window.location.reload()
 });
 
-$("form.comment-class").on("submit", function (e) {
-
-    console.log("banana")
-    let dataString = $(this).serialize();
-    let user_id = $(this).closest("form.comment-class").attr('id');
-    user_id = user_id.replace('comment', '')
-    $.ajax({
-        type: "POST",
-        url: "/make-comment/" + user_id,
-        data: dataString,
-    });
-    e.preventDefault();
-    $("#like_button").load(location.href + " #like_button");
-    window.location.reload()
-});
+// $("form.comment-class").on("submit", function (e) {
+//     console.log("banana")
+//     let dataString = $(this).serialize();
+//     let user_id = $(this).closest("form.comment-class").attr('id');
+//     user_id = user_id.replace('comment', '')
+//     $.ajax({
+//         type: "POST",
+//         url: "/make-comment/" + user_id,
+//         data: dataString,
+//     });
+//     e.preventDefault();
+//     $("#like_button").load(location.href + " #like_button");
+//     window.location.reload()
+// });
